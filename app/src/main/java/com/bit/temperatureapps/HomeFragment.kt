d9f9eed1b3a819977eb9c1e55d6d8f65b0dd4f0b
faryz-coder <faryz.coder@gmail.com>
2021-03-02 21:16:13 +0800

    good doctor apps

---
+++ app/src/main/java/com/bit/temperatureapps/HomeFragment.kt
@@ -0,0 +1,42 @@
+package com.bit.temperatureapps
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ConstraintLayout>(R.id.scanButton).setOnClickListener {
+            findNavController().navigate(R.id.action_HomeFragment_to_ScanFragment)
+        }
+
+        view.findViewById<ConstraintLayout>(R.id.heartButton).setOnClickListener {
+            findNavController().navigate(R.id.action_HomeFragment_to_heartFragment)
+        }
+
+        view.findViewById<ConstraintLayout>(R.id.historyButton).setOnClickListener {
+            findNavController().navigate(R.id.action_HomeFragment_to_HistoryFragment)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/bit/temperatureapps/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.bit.temperatureapps
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed( {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        },1500 )
+    }
+}
